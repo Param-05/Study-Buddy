5f8f17d25d89852a8ac6d52ea6a3e4d6d5b79046
Param Rajguru <phrajguru05@gmail.com>
2025-10-03 01:49:43 -0400

    changed config.ts for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ❗ allow production builds even if there are ESLint errors
+    ignoreDuringBuilds: true,
+  },
+
 };
 
 export default nextConfig;
